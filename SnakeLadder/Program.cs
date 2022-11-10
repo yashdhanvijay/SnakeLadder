53c3ab60876f971fb8f0430794f76473f676fb2a
yashdhanvijay <yash10dhanvijay.com>
2022-11-10 18:49:47 +0530

    [yash]: die roll case completed

--- SnakeLadder/Program.cs
+++ SnakeLadder/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+namespace SnakeLadder
+
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Snake and Ladder Problem Statement");
+
+            GamePlay playerPosition = new GamePlay();
+            
+        }   
+    }
+}
