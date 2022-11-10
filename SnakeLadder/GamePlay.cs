58720b8b51e2db87aa96a5f5131f5550862acc06
yashdhanvijay <yash10dhanvijay.com>
2022-11-10 22:30:15 +0530

    [YASH]:Wining Position case done

--- SnakeLadder/GamePlay.cs
+++ SnakeLadder/GamePlay.cs
@@ -3,11 +3,54 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace SnakeLadder
 {
     public class GamePlay
     {
-        int PlayerPosition = 0;
+        Random random = new Random();
+        const int NO_PLAY = 0, LADDER = 1, SNAKE = 2, WINNING_POSITION = 100;
+        int playerPosition = 0;
+
+        public void Position()
+        {
+            Console.WriteLine("Lets Play!!!" + "\t" + "You are at - " + playerPosition);
+        }
+        public int DiceRoll()
+        {
+            int dice = random.Next(1, 7);
+            Console.WriteLine("Output of Dice - " + dice);
+            _ = this.playerPosition;
+            return dice;
+        }
+        public void Check()
+        {
+            while (playerPosition < WINNING_POSITION)
+            {
+                int ROLL = random.Next(0, 3);
+                Console.WriteLine(ROLL);
+                switch (ROLL)
+                {
+                    case NO_PLAY:
+                        this.playerPosition += 0;
+                        break;
+                    case LADDER:
+                        int die = DiceRoll();
+                        if (this.playerPosition + die <= WINNING_POSITION)
+                            this.playerPosition += die;
+                        break;
+                    case SNAKE:
+                        this.playerPosition -= DiceRoll();
+                        if (this.playerPosition < 0)
+                        {
+                            this.playerPosition = 0;
+                        }
+                        break;
+                }
+                Console.WriteLine("You are at " + this.playerPosition);
+            }
+            Console.WriteLine("Congratulations! You won the Game..!");
+        }
     }
 }
--- SnakeLadder/Program.cs
+++ SnakeLadder/Program.cs
@@ -9,7 +9,12 @@ namespace SnakeLadder
             Console.WriteLine("Welcome to Snake and Ladder Problem Statement");
 
             GamePlay playerPosition = new GamePlay();
-            
-        }   
+            playerPosition.Position();
+            GamePlay check = new GamePlay();
+            check.Check();
+            GamePlay dice = new GamePlay();
+            dice.DiceRoll();
+
+        }
     }
 }
